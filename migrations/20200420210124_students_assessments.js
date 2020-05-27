adaa8b38753e5cee6942c7053b1305be3258bd79
Kelly Keating <keating.kel@gmail.com>
2020-05-28 09:49:05 +1200

    fix migration

--- migrations/20200420210124_students_assessments.js
+++ migrations/20200420210124_students_assessments.js
@@ -2,7 +2,7 @@ exports.up = function(knex) {
     return knex.schema.createTable('students_assessments', table => {
       table.increments('id')
       table.integer('student_id')
-      table.integer('assessment_code')
+      table.string('assessment_code')
       table.string('status')
     })
   };
