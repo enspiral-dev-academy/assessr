452a66617a69c587c5c5f0d597d6af5d4072297c
Emily Coco Parkes <emilycoco@me.com>
2020-07-21 15:56:04 +1200

    throws error if no user_id found

--- server/db/students.js
+++ server/db/students.js
@@ -12,8 +12,12 @@ function getStudentInfo (id, testDb) {
     const db = testDb || connection
 
     return db('students_assessments')
-        .select('assessment_code AS code', 'status', 'students_assessments.id AS assessment_record')
-        .where('user_id', id)
+        .where('user_id', id).first()
+        .then(id => {
+            if (!id) throw new Error('ID doesn\'t exist')
+            return db('students_assessments')
+            .select('assessment_code AS code', 'status', 'students_assessments.id AS assessment_record')
+        }) 
 }
 
 module.exports = {
