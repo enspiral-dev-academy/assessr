a4023541db876d1036bd576f584023ed732a5ac2
Kelly Keating <keating.kel@gmail.com>
2020-08-14 16:19:51 +1200

    add indenting

--- client/reducers/error.js
+++ client/reducers/error.js
@@ -3,7 +3,7 @@ const initialState = ''
 export default function error (state = initialState, action) {
   switch (action.type) {
       case 'SHOW_ERROR':
-      return action.message
+        return action.message
     default:
       return state
   }
